2f7ddd843b8dc3b3dad89daf413b4470269e132c
Eric <eric-213@hotmail.com>
2021-02-27 13:35:09 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// $(document).ready(function () {
+//   $("button").click(function () {
+//     $("p").hide();
+//   });
+// });
+
+console.log("firstWeb");
+
+var handleClick = function (event) {
+  console.log("handleClicked");
+  var xhr = new XMLHttpRequest();
+  xhr.open("post", "/echo/json/", true);
+
+  // xhr.onreadystatechange = function () {
+  //   console.log(this.readyState);
+  // };
+  // xhr.onload = function () {
+  //   console.log(request)
+  //   if (this.status == 200) {
+  //     console.log(this.responseText);
+  //   }
+  // };
+
+  xhr.send();
+};
+
+var button = document.querySelector("#big-button");
+button.addEventListener("click", handleClick);
